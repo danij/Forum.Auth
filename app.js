faef5e60da35cd7f88fc456f139218a732513ad5
Dani J <dani.user@gmail.com>
2018-01-03 21:58:56 +0200

    Reformated sources

--- app.js
+++ app.js
@@ -10,24 +10,24 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use('/', index);
 app.use('/double_submit_cookie', double_submit_cookie);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // only provide error in development
-  res.status(err.status || 500);
-  res.send(req.app.get('env') === 'development' ? err : 'error');
+app.use(function (err, req, res, next) {
+    // only provide error in development
+    res.status(err.status || 500);
+    res.send(req.app.get('env') === 'development' ? err : 'error');
 });
 
 module.exports = app;
