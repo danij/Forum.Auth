2d744accb67f9f657ae1b0360d5e19a38e6efa02
Dani J <dani.user@gmail.com>
2018-01-03 21:06:00 +0200

    Set up a basic express app

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // only provide error in development
+  res.status(err.status || 500);
+  res.send(req.app.get('env') === 'development' ? err : 'error');
+});
+
+module.exports = app;
