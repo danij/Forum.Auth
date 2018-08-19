6afad5e81ebc2e1bc3c4fee5bc6d0cd2943d01f5
Dani J <dani.user@gmail.com>
2018-08-19 13:32:01 +0300

    Generate a new double submit cookie on each request

--- routes/double_submit_cookie.js
+++ routes/double_submit_cookie.js
@@ -11,17 +11,9 @@ function generateNewValue() {
     return crypto.randomBytes(COOKIE_BYTES_LENGTH).toString('hex');
 }
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
 
-    let value;
-
-    if (req.cookies && req.cookies[COOKIE_NAME] && req.cookies[COOKIE_NAME].length) {
-
-        value = req.cookies[COOKIE_NAME];
-    }
-    else {
-        value = generateNewValue();
-    }
+    const value = generateNewValue();
 
     res.cookie(COOKIE_NAME, value, {'httpOnly': true});
     res.send({doubleSubmit: value});
