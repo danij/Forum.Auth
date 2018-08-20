b1c2f3e41f1a68f08de0e31997e5433fdfffba39
Dani J <dani.user@gmail.com>
2018-08-20 22:03:31 +0300

    Forward auth data to the main service

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const crypto = require('crypto');
 const router = express.Router();
+const axios = require('axios');
+const crypto = require('crypto');
 
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
@@ -40,17 +41,42 @@ function hashId(input) {
 
 const redirectCookieName = 'login_redirect';
 const expectedReferer = process.env.EXPECTED_REFERER;
+const authCookieSize = 16;
+const authCookieName = 'auth';
+const authDurationSeconds = parseInt(process.env.AUTH_SECONDS);
+const authRegisterUrl = process.env.AUTH_REGISTER_URL;
 
-function getToken(req, res) {
+function generateRandomToken() {
+
+    return crypto.randomBytes(authCookieSize).toString('hex');
+}
+
+async function getToken(req, res, next) {
 
     const redirectTo = req.cookies[redirectCookieName];
 
     res.cookie(redirectCookieName, '', {maxAge: 0});
 
-    res.send({
-        id: req.user.provider + '_' + hashId(req.user.id),
-        redirectTo: redirectTo
-    });
+    const authToken = generateRandomToken();
+    const authId = req.user.provider + '_' + hashId(req.user.id);
+
+    try {
+        const response = await axios.post(authRegisterUrl +
+            ['login', authToken, authId, authDurationSeconds.toString()].map(encodeURIComponent).join('/'));
+
+        if (response.status >= 200 && response.status < 300) {
+
+            res.cookie(authCookieName, authToken, {maxAge: authDurationSeconds * 1000, httpOnly: true});
+            res.redirect(redirectTo);
+        }
+        else {
+            next(new Error(res.statusText));
+        }
+    }
+    catch(e) {
+
+        next(e);
+    }
 }
 
 router.get('/', (req, res, next) => {
